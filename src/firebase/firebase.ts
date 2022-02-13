46ee23e08757a48b5cb0b45f1a74b160145041e5
Emma Troxell <troxellemma@gmail.com>
2022-02-13 17:57:48 -0600

    update firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,40 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase, query, ref } from 'firebase/database';
-import {
-  GoogleAuthProvider,
-  getAuth,
-  signInWithPopup,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  sendPasswordResetEmail,
-  signOut,
-} from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDf6W8tb_c2bixbgC-5vcVsvgkUM3LWMJc",
-  authDomain: "astro-react-c12a5.firebaseapp.com",
-  projectId: "astro-react-c12a5",
-  storageBucket: "astro-react-c12a5.appspot.com",
-  messagingSenderId: "217366656751",
-  appId: "1:217366656751:web:008a7e61f64382d44c4d1a",
-  measurementId: "G-ZY653BQKF3"
-};
+  apiKey: 'AIzaSyDf6W8tb_c2bixbgC-5vcVsvgkUM3LWMJc',
+  authDomain: 'astro-react-c12a5.firebaseapp.com',
+  projectId: 'astro-react-c12a5',
+  storageBucket: 'astro-react-c12a5.appspot.com',
+  messagingSenderId: '217366656751',
+  appId: '1:217366656751:web:008a7e61f64382d44c4d1a',
+  measurementId: 'G-ZY653BQKF3',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const auth = getAuth(app);
+export const app = initializeApp(firebaseConfig)
 
-// functions
-// const snapshotToArray = (snapshot: any) => Object.entries(snapshot).map(e => Object.assign(e[1], { key: e[0] }));
-
-// export const getUsers = (): User[] => {
-//   const usersRef = ref(db, 'users');
-//   return snapshotToArray(usersRef).map((user: any) => {
-//     return {
-//       id: user.snapshot.key,
-//       email: user.snapshot.val().email
-//     } as User;
-//   });
-// };
 
