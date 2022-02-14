75d8f7f783cbb35ad5eaa66b042dd2185477e720
Emma Troxell <troxellemma@gmail.com>
2022-02-13 19:51:23 -0600

    delete Button.tsx

--- src/components/Button/Button.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-import { Button } from './Button'
-
-test('button should renders', () => {
-  const { getByText } = render(<Button>ButtonContent</Button>)
-
-  expect(getByText('ButtonContent')).toBeTruthy()
-  expect(getByText('ButtonContent')).toHaveAttribute('type', 'button')
-})
