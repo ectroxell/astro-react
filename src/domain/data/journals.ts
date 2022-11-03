85ea8deed0e40041091103269e8f874af1f376c7
Emma.Troxell <emma.troxell@wwt.com>
2022-11-03 15:24:58 -0500

    implement getJournalsById

--- src/domain/data/journals.ts
+++ src/domain/data/journals.ts
@@ -2,18 +2,21 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../firebase/firebase";
 import { Journal } from "../types/Journal";
 
-
-
 export const getJournalsByUserId = async (userId: string) => {
   const journals: Journal[] = [];
   const q = query(collection(db, "journals"), where("userId", "==", userId));
 
   const querySnapshot = await getDocs(q);
   const journalList = querySnapshot.docs.map(doc => doc.data());
-  journalList.forEach(journal => {
-    console.log({journal})
+  journalList.forEach(journalDoc => {
+    const journal: Journal = {
+      date: journalDoc.date.toDate(),
+      moonPhase: journalDoc.moonPhase,
+      text: journalDoc.text,
+      userId: journalDoc.userId,
+    }
     journals.push(journal)
-  })
-  console.log({journals})
+  });
+
   return journals;
 };
