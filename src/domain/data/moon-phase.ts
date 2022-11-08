0ef0c5c5e9d14e3ed2ecb3c446daffa4a1263d3d
Emma.Troxell <emma.troxell@wwt.com>
2022-11-08 09:23:58 -0600

    Update fetchMoonData to be more realistic

--- src/domain/data/moon-phase.ts
+++ src/domain/data/moon-phase.ts
@@ -2,7 +2,8 @@ import { MoonData } from "../types/MoonData";
 import SunCalc from 'suncalc';
 
 export const fetchMoonData = () => {
-  const rawMoonData = SunCalc.getMoonIllumination(new Date());
+  const date = new Date();
+  const rawMoonData = SunCalc.getMoonIllumination(date);
   const phase = getMoonPhase(rawMoonData.phase);
   const moonData: MoonData = {illuminated: Math.round(rawMoonData.fraction * 100), phase}
   return moonData;
@@ -10,21 +11,21 @@ export const fetchMoonData = () => {
 
 const getMoonPhase = (phaseAsFraction: number) => {
   let moonPhase;
-  if (phaseAsFraction === 0) {
+  if (phaseAsFraction >= 0 && phaseAsFraction <= 0.05) {
     moonPhase = "New Moon";
-  } else if (phaseAsFraction === 0.25) {
+  } else if (phaseAsFraction >= 0.25 && phaseAsFraction <= 0.3) {
     moonPhase = "First Quarter";
-  } else if (phaseAsFraction === 0.5) {
+  } else if (phaseAsFraction >= 0.5 && phaseAsFraction <= 0.55) {
     moonPhase = "Full Moon";
-  } else if (phaseAsFraction === 0.75) {
+  } else if (phaseAsFraction >= 0.75 && phaseAsFraction <= 0.8) {
     moonPhase = "Last Quarter";
-  } else if (phaseAsFraction > 0 && phaseAsFraction < 0.25) {
+  } else if (phaseAsFraction > 0.05 && phaseAsFraction < 0.25) {
     moonPhase = "Waxing Crescent";
-  } else if (phaseAsFraction > 0.25 && phaseAsFraction < 0.5) {
+  } else if (phaseAsFraction > 0.3 && phaseAsFraction < 0.5) {
     moonPhase = "Waxing Gibbous";
-  } else if (phaseAsFraction > 0.5 && phaseAsFraction < 0.75) {
+  } else if (phaseAsFraction > 0.55 && phaseAsFraction < 0.75) {
     moonPhase = "Waning Gibbous";
-  } else if (phaseAsFraction > 0.75 && phaseAsFraction < 1) {
+  } else if (phaseAsFraction > 0.8 && phaseAsFraction < 1) {
     moonPhase = "Waning Crescent";
   } else {
     throw(new Error('Invalid value'))
