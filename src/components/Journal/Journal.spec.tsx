37226cee45570a77b4ca2a6c16c4ac2da740e5e7
Emma.Troxell <emma.troxell@wwt.com>
2022-11-10 15:58:15 -0600

    add JournalPage test cases

--- src/components/Journal/Journal.spec.tsx
+++ src/components/Journal/Journal.spec.tsx
@@ -13,9 +13,33 @@ describe('JournalPage', () => {
       userId: '12',
     },
   ]
-  it('should render header', () => {
-    const { getByText } = render(<JournalPage userName={name} journals={journals}/>)
+  const emptyJournal: Journal[] = []
+  it('should render header journals for signed in user with journal entries', () => {
+    const { getByText } = render(
+      <JournalPage userName={name} journals={journals} />
+    )
 
     expect(getByText(`${name}'s Moon Journal 🌙`)).toBeInTheDocument()
+    expect(getByText(journals[0].text)).toBeInTheDocument()
+  })
+
+  it('should render no journals message for signed in users with zerio journal entries', () => {
+    const { getByText } = render(
+      <JournalPage userName={name} journals={emptyJournal} />
+    )
+
+    expect(
+      getByText(
+        'You do not have any journal entries. Create your first journal entry here!'
+      )
+    ).toBeInTheDocument()
+  })
+
+  it('should render correct message for logged out user', () => {
+    const { getByText } = render(
+      <JournalPage userName={null} journals={emptyJournal} />
+    )
+
+    expect(getByText('Login to create a journal entry.')).toBeInTheDocument()
   })
 })
