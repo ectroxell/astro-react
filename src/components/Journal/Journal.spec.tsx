3f812a2ad7d1fb5e5ee0d91242820f1cb567921a
Emma.Troxell <emma.troxell@wwt.com>
2022-11-16 11:06:13 -0600

    WIP try to fix journal test

--- src/components/Journal/Journal.spec.tsx
+++ src/components/Journal/Journal.spec.tsx
@@ -3,7 +3,7 @@ import { Journal } from '../../domain/types/Journal'
 import { JournalPage } from './Journal'
 
 describe('JournalPage', () => {
-  const user = {displayName: 'Albert', uid: '123'}
+  const user = { displayName: 'Albert', uid: '123' }
   const journals: Journal[] = [
     {
       text: 'this is my journal',
@@ -14,20 +14,46 @@ describe('JournalPage', () => {
     },
   ]
   const emptyJournal: Journal[] = []
-  const mockUpdateJournals = jest.fn()
+  const moonPhase: string = 'full moon'
+  
+  const mockUpdateJournals = jest.fn().mockImplementation((journals, newJournal) => {
+    return [...journals, newJournal]
+  })
+
+  jest.mock('../../domain/data/journals', () => {
+    const originalModule = jest.requireActual('../../domain/data/journals')
+
+    return {
+      __esModule: true,
+      ...originalModule,
+      createNewJournal: jest.fn(() => 'created new journal'),
+    }
+  })
 
   it('should render header journals for signed in user with journal entries', () => {
     const { getByText } = render(
-      <JournalPage user={user} journals={journals} currentMoonPhase={'full moon'} updateJournals={mockUpdateJournals} />
+      <JournalPage
+        user={user}
+        journals={journals}
+        currentMoonPhase={moonPhase}
+        updateJournals={mockUpdateJournals}
+      />
     )
 
-    expect(getByText(`${user.displayName}'s Moon Journal 🌙`)).toBeInTheDocument()
+    expect(
+      getByText(`${user.displayName}'s Moon Journal 🌙`)
+    ).toBeInTheDocument()
     expect(getByText(journals[0].text)).toBeInTheDocument()
   })
 
   it('should render no journals message for signed in users with zerio journal entries', () => {
     const { getByText } = render(
-      <JournalPage user={user} journals={emptyJournal} currentMoonPhase={'full moon'} updateJournals={mockUpdateJournals} />
+      <JournalPage
+        user={user}
+        journals={emptyJournal}
+        currentMoonPhase={moonPhase}
+        updateJournals={mockUpdateJournals}
+      />
     )
 
     expect(
@@ -39,7 +65,12 @@ describe('JournalPage', () => {
 
   it('should render correct message for logged out user', () => {
     const { getByText } = render(
-      <JournalPage user={null} journals={emptyJournal} currentMoonPhase={'full moon'} updateJournals={mockUpdateJournals} />
+      <JournalPage
+        user={null}
+        journals={emptyJournal}
+        currentMoonPhase={moonPhase}
+        updateJournals={mockUpdateJournals}
+      />
     )
 
     expect(getByText('Login to create a journal entry.')).toBeInTheDocument()
@@ -47,12 +78,17 @@ describe('JournalPage', () => {
 
   it('should open modal when create new entry button is clicked and close when X button is clicked', () => {
     const { getByText, getByRole } = render(
-      <JournalPage user={user} journals={journals} currentMoonPhase={'full moon'} updateJournals={mockUpdateJournals} />
+      <JournalPage
+        user={user}
+        journals={journals}
+        currentMoonPhase={moonPhase}
+        updateJournals={mockUpdateJournals}
+      />
     )
 
     expect(getByText('New Journal Entry')).not.toBeVisible()
-    
-    const newEntryButton = getByRole('button', {name: 'New Entry'})
+
+    const newEntryButton = getByRole('button', { name: 'New Entry' })
     fireEvent.click(newEntryButton)
 
     expect(getByText('New Journal Entry')).toBeVisible()
@@ -65,25 +101,31 @@ describe('JournalPage', () => {
 
   // it('should add new journal and close modal when submit button is pressed', async () => {
   //   const { getByText, getByRole, findByText } = render(
-  //     <JournalPage user={user} journals={journals} currentMoonPhase={'full moon'} updateJournals={mockUpdateJournals} />
+  //     <JournalPage
+  //       user={user}
+  //       journals={journals}
+  //       currentMoonPhase={moonPhase}
+  //       updateJournals={mockUpdateJournals}
+  //     />
   //   )
   //   const journalText = 'this is my newest journal entry'
+
   //   expect(getByText('New Journal Entry')).not.toBeVisible()
-    
-  //   const newEntryButton = getByRole('button', {name: 'New Entry'})
+
+  //   const newEntryButton = getByRole('button', { name: 'New Entry' })
   //   fireEvent.click(newEntryButton)
 
   //   expect(getByText('New Journal Entry')).toBeVisible()
 
   //   const inputBox = getByRole('textbox')
-  //   const submitButton = getByRole('button', {name: 'Submit'})
-
+  //   const submitButton = getByRole('button', { name: 'Submit' })
   //   fireEvent.click(inputBox)
-  //   fireEvent.change(inputBox, {target: {value: journalText}})
+  //   fireEvent.change(inputBox, { target: { value: journalText } })
   //   fireEvent.click(submitButton)
+
   //   await waitFor(async () => {
   //     expect(await findByText('New Journal Entry')).not.toBeVisible()
+  //     expect(await findByText(journalText)).toBeInTheDocument()
   //   })
-  //   expect(await findByText(journalText)).toBeInTheDocument()
   // })
 })
