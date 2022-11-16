95661bd6c963ca4feb3226515c0944877d4b0e69
Emma.Troxell <emma.troxell@wwt.com>
2022-11-16 11:45:20 -0600

    skip failing test

--- src/components/Journal/Journal.spec.tsx
+++ src/components/Journal/Journal.spec.tsx
@@ -99,33 +99,33 @@ describe('JournalPage', () => {
     expect(getByText('New Journal Entry')).not.toBeVisible()
   })
 
-  // it('should add new journal and close modal when submit button is pressed', async () => {
-  //   const { getByText, getByRole, findByText } = render(
-  //     <JournalPage
-  //       user={user}
-  //       journals={journals}
-  //       currentMoonPhase={moonPhase}
-  //       updateJournals={mockUpdateJournals}
-  //     />
-  //   )
-  //   const journalText = 'this is my newest journal entry'
+  it.skip('should add new journal and close modal when submit button is pressed', async () => {
+    const { getByText, getByRole, findByText } = render(
+      <JournalPage
+        user={user}
+        journals={journals}
+        currentMoonPhase={moonPhase}
+        updateJournals={mockUpdateJournals}
+      />
+    )
+    const journalText = 'this is my newest journal entry'
 
-  //   expect(getByText('New Journal Entry')).not.toBeVisible()
+    expect(getByText('New Journal Entry')).not.toBeVisible()
 
-  //   const newEntryButton = getByRole('button', { name: 'New Entry' })
-  //   fireEvent.click(newEntryButton)
+    const newEntryButton = getByRole('button', { name: 'New Entry' })
+    fireEvent.click(newEntryButton)
 
-  //   expect(getByText('New Journal Entry')).toBeVisible()
+    expect(getByText('New Journal Entry')).toBeVisible()
 
-  //   const inputBox = getByRole('textbox')
-  //   const submitButton = getByRole('button', { name: 'Submit' })
-  //   fireEvent.click(inputBox)
-  //   fireEvent.change(inputBox, { target: { value: journalText } })
-  //   fireEvent.click(submitButton)
+    const inputBox = getByRole('textbox')
+    const submitButton = getByRole('button', { name: 'Submit' })
+    fireEvent.click(inputBox)
+    fireEvent.change(inputBox, { target: { value: journalText } })
+    fireEvent.click(submitButton)
 
-  //   await waitFor(async () => {
-  //     expect(await findByText('New Journal Entry')).not.toBeVisible()
-  //     expect(await findByText(journalText)).toBeInTheDocument()
-  //   })
-  // })
+    await waitFor(async () => {
+      expect(await findByText('New Journal Entry')).not.toBeVisible()
+      expect(await findByText(journalText)).toBeInTheDocument()
+    })
+  })
 })
